778bc50056111f6a0f43af7afa09a7207a04da6f
FisumTeshome <teshfitsum834@gmail.com>
2024-10-23 23:09:34 +0300

    try for second deployment

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -13,8 +13,8 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}> {/* Wrap App with Provider and pass in the store */}
-      <BrowserRouter> {/* Wrap App with BrowserRouter for routing */}
+    <Provider store={store}> 
+      <BrowserRouter basename="/MusicManager">
         <App />
       </BrowserRouter>
     </Provider>
